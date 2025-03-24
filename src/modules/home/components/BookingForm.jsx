11243bdb6e1dce52efc61f8fc245b400848958e6
Johan <johannuseche2020@gmail.com>
2025-03-23 19:22:15 -0500

    section-complete

--- src/modules/home/components/BookingForm.jsx
+++ src/modules/home/components/BookingForm.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export const BookingForm = () => {
   return (
     <div className="relative z-10 bg-white rounded-xl shadow-lg p-5 md:p-6 flex flex-wrap md:flex-nowrap items-center mx-auto mt-[-47px] space-y-4 md:space-y-0 md:space-x-4" style={{ width: '84%' }}>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 w-full items-center gap-4">
+      <div className="grid md:flex w-full items-center gap-4">
         {/* Habitación */}
-        <div className="flex flex-col items-center md:items-start gap-2 w-full">
+        <div className="flex items-center gap-2 w-full md:w-1/5">
           <i className="fas fa-bed text-teal-500 text-lg"></i>
-          <div className="flex flex-col w-full text-center md:text-left">
-            <span className="text-gray-900 font-medium text-[16px]">Habitación</span>
-            <select className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1 sm:max-w-xs mx-auto md:mx-0">
+          <div className="flex flex-col ml-9">
+            <span className="ttext-gray-900  font-medium text-[16px]">Habitación</span>
+            <select className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1">
               <option>Standard</option>
               <option>Deluxe</option>
               <option>Suite</option>
@@ -18,11 +18,11 @@ export const BookingForm = () => {
         </div>
 
         {/* Huéspedes */}
-        <div className="flex flex-col items-center md:items-start gap-2 w-full">
+        <div className="flex items-center gap-2 w-full md:w-1/4">
           <i className="fas fa-user text-teal-500 text-lg"></i>
-          <div className="flex flex-col w-full text-center md:text-left">
-            <span className="text-gray-900 font-medium text-[16px]">Huéspedes</span>
-            <select className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1 sm:max-w-xs mx-auto md:mx-0">
+          <div className="flex flex-col ml-17">
+            <span className="text-gray-900  font-medium text-[16px]">Huéspedes</span>
+            <select className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1">
               <option>01</option>
               <option>02</option>
               <option>03</option>
@@ -32,25 +32,25 @@ export const BookingForm = () => {
         </div>
 
         {/* Check-in */}
-        <div className="flex flex-col items-center md:items-start gap-2 w-full">
+        <div className="flex items-center gap-2 w-full md:w-1/5">
           <i className="fas fa-calendar-check text-teal-500 text-lg"></i>
-          <div className="flex flex-col w-full text-center md:text-left">
-            <span className="text-gray-900 font-medium text-[16px]">Check-in</span>
+          <div className="flex flex-col">
+            <span className="text-gray-900  font-medium text-[16px]">Check-in</span>
             <input
               type="date"
-              className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1 sm:max-w-xs mx-auto md:mx-0"
+              className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1"
             />
           </div>
         </div>
 
         {/* Check-out */}
-        <div className="flex flex-col items-center md:items-start gap-2 w-full">
+        <div className="flex items-center gap-2 w-full md:w-1/5">
           <i className="fas fa-calendar-alt text-teal-500 text-lg"></i>
-          <div className="flex flex-col w-full text-center md:text-left">
-            <span className="text-gray-900 font-medium text-[16px]">Check-out</span>
+          <div className="flex flex-col">
+            <span className="text-gray-900  font-medium text-[16px]">Check-out</span>
             <input
               type="date"
-              className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1 sm:max-w-xs mx-auto md:mx-0"
+              className="bg-white text-gray-500 text-xs font-medium rounded-md border-gray-300 focus:outline-none p-1"
             />
           </div>
         </div>
@@ -58,7 +58,7 @@ export const BookingForm = () => {
         {/* Botón de Reservar ahora */}
         <Link
           to="/buscar"
-          className="bg-[#34A0A4] text-18 text-white px-6 py-3 rounded-2xl w-full md:w-1/5 flex items-center justify-center gap-2 shadow-md mt-2 md:mt-0 whitespace-nowrap font-dm-sans tracking-wide"
+          className="bg-[#34A0A4]  text-18 text-white px-6 py-3 rounded-2xl  w-full md:w-1/5 flex items-center justify-center gap-2 shadow-md mt-2 md:mt-0  whitespace-nowrap font-dm-sans tracking-wide"
         >
           <i className="fas fa-search text-lg"></i> Reservar ahora
         </Link>
--- src/modules/home/components/Testimonials.jsx
+++ src/modules/home/components/Testimonials.jsx
@@ -3,11 +3,44 @@ import { Link } from "react-router-dom";
 
 export const Testimonials = () => {
   return (
-    <section className="w-full bg-white py-12 flex flex-col mt-18 items-center text-center overflow-hidden">
-      <h2 className="text-4xl font-bold text-gray-800 mb-6 mt-4" >
-        Lo Que Opinan Nuestros Clientes.
-      </h2>
+    
+        <div className="w-[900px] flex flex-col justify-center items-center mt-12 gap-10 mx-auto">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <div className="text-center text-siah text-4xl whitespace-nowrap font-semibold font-dm-sans tracking-wide">
+              Lo Que Opinan Nuestros Clientes.
+            </div>
+          </div>
+          <div className="self-stretch pt-10 pb-2 flex flex-col justify-center items-center mt-10  gap-6 overflow-hidden">
+            <div className="w-[1400px] h-44 relative flex justify-center items-center">
+              <div className="w-[400px] h-44 px-6 pt-12 pb-6 absolute bg-white rounded-[40px] shadow-[0px_0px_15px_0px_rgba(0,0,0,0.15)] flex flex-col justify-start items-center gap-6 left-[50px] top-[10px]">
+                <div className="w-36 text-center text-black text-lg font-bold font-['Poppins']">Cameron Williamson</div>
+                <div className="w-[380px] text-center text-black text-lg font-light font-['Poppins']">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id es</div>
+                <img className="w-8 h-16 left-[192px] top-[-35px] absolute rounded-[40px]" src="public/testimonio1.png" />
+              </div>
+              <div className="w-[480px] h-44 px-6 pt-12 pb-6 absolute bg-white rounded-[40px] shadow-[0px_0px_15px_0px_rgba(0,0,0,0.15)] flex flex-col justify-start items-center gap-6 left-[480px] top-0">
+                <div className="w-36 text-center text-black text-lg font-semibold font-['Poppins']">Fatima Taylor</div>
+                <div className="w-[380px] text-center text-black text-xl font-normal font-['Poppins']">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui</div>
+                <img className="w-16 h-16 left-[206px] top-[-35px] absolute rounded-[40px]" src="public/testimonio2.png" />
+              </div>
+              <div className="px-6 pt-12 pb-6 absolute bg-white rounded-[40px] shadow-[0px_0px_15px_0px_rgba(0,0,0,0.15)] flex flex-col justify-start items-center gap-6 left-[990px] top-[12px]">
+                <div className="w-36 text-center text-black text-lg font-bold font-['Poppins']">Sara Watson</div>
+                <div className="w-[380px] text-center text-black text-lg font-light font-['Poppins']">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id es</div>
+                <img className="w-16 h-16 left-[180px] top-[-35px] absolute rounded-[40px]" src="public/testimnio3.png" />
+              </div>
+            </div>
+            
 
-    </section>
-  );
-};
+            <div class="flex justify-center items-center gap-3 ml-5 mt-8 mb-20">
+    <div class="w-3 h-3 bg-gray-800 rounded-full"></div>
+    <div class="w-3 h-3 bg-gray-800 rounded-full"></div>
+    <div class="w-9 h-3 bg-teal-600 rounded-full"></div>
+    <div class="w-3 h-3 bg-gray-800 rounded-full"></div>
+</div>
+
+
+
+          </div>
+        </div>
+      );
+    }
+    
