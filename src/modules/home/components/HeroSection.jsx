8310ed622cddf64dfd787fd2b283893c7dab856e
Johan <johannuseche2020@gmail.com>
2025-03-24 18:02:17 -0500

    implementacion-responsivev3

--- src/modules/home/components/HeroSection.jsx
+++ src/modules/home/components/HeroSection.jsx
@@ -19,19 +19,19 @@ export const HeroSection = () => {
       {/* Botón Ver Video */}
       <a 
        
-        className="absolute left-43 transform -translate-x-1/2 bottom-24 sm:bottom-16 md:bottom-24 flex items-center gap-4 sm:gap-6 cursor-pointer"
+        className="absolute left-43 transform -translate-x-1/2 bottom-24 sm:bottom-16 md:bottom-24 flex items-center gap-4 sm:gap-6 "
       >
-        <div className="text-white text-lg sm:text-xl md:text-2xl hitespace-nowrap font-dm-sans tracking-wide">
+        <div className="text-white text-lg sm:text-xl md:text-2xl hitespace-nowrap font-dm-sans  tracking-wide">
           Ver video
         </div>
-        <div className="relative flex items-center justify-center">
+        <div className="relative flex items-center justify-center cursor-pointer">
           {/* Círculos de borde */}
-          <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 absolute rounded-full border border-white/20"></div>
-          <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 absolute rounded-full border border-white/30"></div>
+          <div className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 absolute rounded-full border  cursor-pointer border-white/20"></div>
+          <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 absolute rounded-full border cursor-pointer  border-white/30"></div>
           
           {/* Icono de Play */}
-          <div className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 p-2 bg-white/0 rounded-full outline outline-1 outline-offset-[-1px] outline-white/60 flex items-center justify-center">
-            <img src="public/play_arrow.png" alt="Play Icon" className="w-6 h-6" />
+          <div className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 p-2 bg-white/0 rounded-full outline cursor-pointer outline-1 outline-offset-[-1px] outline-white/60 flex items-center justify-center">
+            <img src="public/play_arrow.png" alt="Play Icon" className="w-6 h-6 cursor" />
           </div>
         </div>
       </a>
--- src/modules/home/components/PopularRooms.jsx
+++ src/modules/home/components/PopularRooms.jsx
@@ -4,47 +4,46 @@ import { rooms } from "../mock/mockRooms";
 export const PopularRooms = () => {
   return (
     <section className="py-[59px] px-6 relative w-[84%] mx-auto">
-      {/* Título Principal */}
+      
       <h2 className="text-[40px] font-medium text-center section-title mb-[59px] w-full max-w-[410px] mx-auto whitespace-nowrap font-dm-sans tracking-wide">
         Habitaciones Populares
       </h2>
 
       <div className="relative flex items-center justify-center">
-        {/* Botón Izquierdo */}
+      
         <img 
           src="/left.png"  
-          className="absolute left-[-21px] top-33 transform -translate-y-1/2 z-10 w-12 h-12 cursor-pointer hidden md:block"
+          className="absolute left-[-24px] top-33 transform -translate-y-1/2 z-10 w-12 h-12 cursor-pointer hidden md:block"
         />
 
         {/* Contenedor de habitaciones */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-[2px] px-8">
           {rooms.map((room) => (
             <div key={room.id} className="bg-white flex flex-col items-start p-4 rounded-lg">
-              {/* Imagen con bordes redondeados */}
+             
               <img 
                 src={room.img} 
                 className="rounded-xl w-full h-auto object-cover mb-4" 
                 alt={room.name} 
               />
 
-              {/* Nombre de la habitación */}
+             
               <h3 className="text-[17px] font-medium font-dm-sans tracking-wide w-full mb-1">
                 {room.name}
               </h3>
 
-              {/* Descripción de la habitación */}
+    
               <p className="text-[15px] text-[#858585] font-dm-sans leading-snug w-full mb-4">
                 {room.description}
               </p>
 
-              {/* Precio + Horario alineado correctamente */}
+              
               <div className="flex items-center">
-                {/* Precio */}
+           
                 <p className="text-[27px] font-normal text-[#34A0A4] font-dm-sans tracking-wide">
                   {room.price}
                 </p>
 
-                {/* Horario con mejor separación */}
                 <p className="text-[15px] font-normal text-[#1A759F] font-dm-sans tracking-wide ml-2">
                   {room.horario}
                 </p>
@@ -53,10 +52,10 @@ export const PopularRooms = () => {
           ))}
         </div>
 
-        {/* Botón Derecho */}
+       
         <img 
           src="/right.png"  
-          className="absolute right-[-21px] top-33 transform -translate-y-1/2 z-10 w-12 h-12 cursor-pointer hidden md:block"
+          className="absolute right-[-24px] top-33 transform -translate-y-1/2 z-10 w-12 h-12 cursor-pointer hidden md:block"
         />
       </div>
     </section>
