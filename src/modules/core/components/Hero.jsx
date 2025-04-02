2058ba04a15ef157f9777a30ebde9a366346befc
Johan <johannuseche2020@gmail.com>
2025-04-02 10:40:45 -0500

    hero-letter

--- src/modules/core/components/Hero.jsx
+++ src/modules/core/components/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = ({ title, subtitle }) => {
   return (
     <div className="bg-[url('/public/heroV2.jpeg')] bg-no-repeat bg-cover h-[520px] flex justify-center ">
       <div className='self-center text-center text-white'>
-        <h2 className='text-7xl'>{title}</h2>
+        <h2 className='text-7xl font-bold'>{title}</h2>
         <p className='text-lg'>{subtitle}</p>
       </div>
     </div>
