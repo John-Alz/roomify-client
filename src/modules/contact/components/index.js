9abf7ecc6f92d01caac522bd7a7bc4f661387a51
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-04-06 19:12:10 -0500

    Revert "maquetacion contactos"
    This reverts commit 30dc6c057f625a7807c3569b9328f7f672d38dce.

--- src/modules/contact/components/index.js
+++
@@ -1,4 +0,0 @@
-export  {Navbar} from "./NavBar";
-export {HeroContact} from "./HeroContact";
-export {ZonaContacto} from "./ZonaContacto";
-export {Footer} from "./Footer";
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../modules/auth/routes/AuthRoutes'
-import { ContactRoutes } from '../modules/contact/rout/ContactRoutes'
 
 export const AppRouter = () => {
     return (
         <Routes>
             <Route path='/auth/*' element={<AuthRoutes />} />
-            <Route path='/' element={<ContactRoutes />} />
         </Routes>
     )
 }
-
