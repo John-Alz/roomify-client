a715c137152d6eb384f6048b38b3bc80bad39efd
Johan <johannuseche2020@gmail.com>
2025-04-22 20:33:46 -0500

    implementacion-Panel-Contact-v3

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from "../modules/auth/routes/AuthRoutes";
 import { HomeRoutes } from "../modules/home/routes/HomeRoutes";
 import { ServicesRoutes } from "../modules/services/routes/ServicesRoutes";
-import { ContactRoutes } from "../modules/Contact/routes/ContactRoutes"; // asegúrate que esta importación exista
+import { ContactRoutes } from "../modules/Contact/routes/ContactRoutes"; 
 
 export const AppRouter = () => {
   return (
@@ -13,7 +13,7 @@ export const AppRouter = () => {
       <Route path="/auth/*" element={<AuthRoutes />} />
       <Route path="/" element={<HomeRoutes />} />
       <Route path="/services/*" element={<ServicesRoutes />} />
-      <Route path="/contact/*" element={<ContactRoutes />} />
+      <Route path="/contact/" element={<ContactRoutes />} />
     </Routes>
   );
 };
